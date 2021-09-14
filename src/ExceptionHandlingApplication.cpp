e159c5c86c9bfbe8993da2e3ffa132b8304e8a0b
Berger <berger@cs.rwth-aachen.de>
2021-09-14 10:28:44 +0200

    Implemented Signal handling for catching and responding to Ctrl+C.

--- src/ExceptionHandlingApplication.cpp
+++ src/ExceptionHandlingApplication.cpp
@@ -8,7 +8,7 @@
 namespace packetlossutils {
 	namespace utility {
 
-		ExceptionHandlingApplication::ExceptionHandlingApplication(int& argc, char** argv) : QCoreApplication(argc, argv) {
+		ExceptionHandlingApplication::ExceptionHandlingApplication(int& argc, char** argv) : QCoreApplication(argc, argv), SignalHandler(SignalHandler::SIG_INT) {
 			// Intentionally left empty.
 		}
 
@@ -21,10 +21,18 @@ namespace packetlossutils {
 			if (baseException != nullptr) {
 				LOGGER()->critical("Caught an exception of type {} in the main program loop with cause: {}", baseException->name(), baseException->what());
 				std::cerr << "Caught an exception of type " << baseException->name() << " in the main program loop with cause: " << baseException->what() << std::endl;
-			} else {
+			} else if (e != nullptr) {
 				LOGGER()->critical("Caught an exception in the main program loop with cause: {}", e->what());
 				std::cerr << "Caught an exception in the main program loop with cause: " << e->what() << std::endl;
+			} else {
+				LOGGER()->critical("Caught a NULLPTR exception in the main program loop, this should never happen?!");
+				std::cerr << "Caught a NULLPTR exception in the main program loop, this should never happen?!" << std::endl;
 			}
+		}
+
+		bool ExceptionHandlingApplication::handleSignal(int signal) {
+			emit onSigInt();
+			return true;
 		}
 
 	}
--- src/ExceptionHandlingApplication.h
+++ src/ExceptionHandlingApplication.h
@@ -5,15 +5,22 @@
 #include <QObject>
 #include <QEvent>
 
+#include "src/utility/SignalHandler.h"
+
 namespace packetlossutils {
 	namespace utility {
 
-		class ExceptionHandlingApplication : public QCoreApplication {
+		class ExceptionHandlingApplication : public QCoreApplication, public SignalHandler {
+			Q_OBJECT
 		public:
 			ExceptionHandlingApplication(int& argc, char** argv);
 			virtual ~ExceptionHandlingApplication();
 
 			static void displayExceptionInfo(std::exception* e);
+
+			virtual bool handleSignal(int signal) override;
+		signals:
+			void onSigInt();
 		};
 
 	}
